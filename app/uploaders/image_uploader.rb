738d2557337161ae90567de40ec3da31d3e67d87
Bong <mjbond_bong2@yahoo.com>
2017-05-03 16:16:19 +0800

    carrierwave working but only 1 picture

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -30,19 +30,19 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  version :thumb do
-    process resize_to_fit: [50, 50]
-  end
+  # version :thumb do
+  #   process resize_to_fit: [50, 50]
+  # end
 
-  version :medium do
-    process resize_to_fit: [300, 300]
-  end
+  # version :medium do
+  #   process resize_to_fit: [300, 300]
+  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
     %w(jpg jpeg gif png)
-  # end
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
